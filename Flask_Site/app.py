25f168ee131af63b90c56bfbd469456f620e46a8
ArTeGaS-2 <probablytorvik@gmail.com>
2025-09-21 16:01:30 +0300

    Дадали файл index.html з розміткою. Почали заповнювати та розбиратись в елементах шаблону.

--- Flask_Site/app.py
+++ Flask_Site/app.py
@@ -1,24 +1,14 @@
 # імпортуємо клас Flask з пакету flask
-from flask import Flask
+from flask import Flask, render_template
 
 # створюємо екземпляр класу. __name__ каже де шукати ресурси
 app = Flask(__name__)
 
 # декоратор route прив'язує URL "/" до функції hello
 @app.route("/")
-def hello():
+def home():
     # повертаємо відповідь у браузері
-    return "привіт"
-
-@app.route("/about")
-def about():
-    return "Про сайт"
-
-@app.route("/user/<name>")
-def user_page(name):
-    return f"Сторінка користувача: {name}"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-#http://127.
