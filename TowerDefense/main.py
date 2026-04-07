84a03b2cf68d05a6e2422267892cfc2e2a7ca96b
ArTeGaS-2 <probablytorvik@gmail.com>
2026-04-07 15:27:33 +0300

    Зафіксували прогрес асинхронної роботи над Tower Defense

--- TowerDefense/main.py
+++ TowerDefense/main.py
@@ -5,3 +5,4 @@ PROJECT_ROOT = Path(__file__).resolve().parent
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
 
+# Технічний коментар щоб уникнути проблем з комітом і пушем на гітхаб
